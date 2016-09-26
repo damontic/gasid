46a7d28db106f237b5b3b186fd036568e7600afa
David Alberto Montaño Fetecua <damontic@gmail.com>
2016-09-25 23:22:35 -0500

    Playing a little with subgraphs

--- bin/python/AwsGenerateDiagram.py
+++ bin/python/AwsGenerateDiagram.py
@@ -8,13 +8,19 @@ import os
 import sys
 
 DIRECTORY_PICKLES = "pickles"
+DIRECTORY_OUTPUT = "output"
 FILE_INSTANCES = "instances.pickle"
 FULL_FILE_INSTANCES_PATH = DIRECTORY_PICKLES+os.sep+FILE_INSTANCES
 INSTANCE_NAME_TAG = "Name"
 FILE_WRITE_BYTES = "wb"
 FILE_READ_BYTES = "rb"
-FILE_DOT = "my_graph.dot"
+FILE_DOT = "graph.dot"
+FULL_FILE_RESULT_PATH = DIRECTORY_OUTPUT+os.sep+FILE_DOT
+ENGINE_FDP = "fdp"
+IMAGE_FORMAT_PNG = "png"
+NODE_ATTRIBUTES = {"image":'icons/ec2_instance.png', "peripheries":"0", "shape":"none"}
 
+ERROR_NO_INSTANCE_NAME_TAG = "Instance has no InstanceName tag"
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -58,23 +64,36 @@ class Gasid(object):
                 return instances
 
         def create_dot_file(self, instances):
-                graph = graphviz.Graph()
+                graph = graphviz.Graph("Principal")
+                graph1 = graphviz.Graph()
+                graph2 = graphviz.Graph()
+
                 for instance in instances:
                         if INSTANCE_NAME_TAG in instance.tags:
-                                graph.node(instance.tags[INSTANCE_NAME_TAG])
+                                graph.node(instance.tags[INSTANCE_NAME_TAG], _attributes=NODE_ATTRIBUTES)
+                                #if instance.vpc_id not in vpcs.keys():
+                                #        vpcs[instance.vpc_id] = []
+                                #else:
+                                #        vpcs[instance.vpc_id].append(instance)
                         else:
-                                eprint("Instance has no InstanceName tag")
+                                eprint(ERROR_NO_INSTANCE_NAME_TAG)
 
                 for instance_a in instances:
                         for instance_b in instances:
                                 if INSTANCE_NAME_TAG in instance_a.tags and INSTANCE_NAME_TAG in instance_b.tags:
                                         graph.edge(instance_a.tags[INSTANCE_NAME_TAG], instance_b.tags[INSTANCE_NAME_TAG])
 
-                graph.save(FILE_DOT)
+                graph1.node("otro nodo 1")
+                graph2.node("otro nodo 2")
+                graph.subgraph(graph1)
+                graph.subgraph(graph2)
+                graph.edge("otro nodo 1", "otro nodo 2")
+                print(graph._subgraph)
+                graph.save(FULL_FILE_RESULT_PATH)
 
-                return FILE_DOT
+                return FULL_FILE_RESULT_PATH
 
-        def create_image(self, engine = "fdp", format = "svg", file_dot = None):
+        def create_image(self, engine = ENGINE_FDP, format = IMAGE_FORMAT_PNG, file_dot = None):
                 graphviz.render(engine, format, file_dot)
 
 if __name__ == "__main__":
