82195616d9e559f42e659aeb77620779f52e2a5d
David Alberto Montaño Fetecua <damontic@gmail.com>
2016-09-25 19:59:00 -0500

    Some corrections.
    - pickles directory and loading from pickles if already got the instances list

--- bin/python/AwsGenerateDiagram.py
+++ bin/python/AwsGenerateDiagram.py
@@ -1,13 +1,46 @@
 #! /bin/python
 
 import boto
+import pickle
+import os
 
-connection	=	boto.connect_ec2()
-regions		=	connection.get_all_regions()
+DIRECTORY_PICKLES = "pickles"
+FILE_INSTANCES = "instances.pickle"
+FULL_FILE_INSTANCES_PATH = DIRECTORY_PICKLES+os.sep+FILE_INSTANCES
+INSTANCE_NAME_TAG = "Name"
+FILE_WRITE_BYTES = "wb"
+FILE_READ_BYTES = "rb"
 
-instances	=	connection.get_only_instances()
-for instance in instances:
-	if 'InstanceName' in instance.tags:
-		print(instance.tags['InstanceName'])
-	else:
-		print("Instance has no InstanceName tag")
+def get_instances():
+        """
+        Returns the instances from AWS and stores them in the pickles directory.
+        If the pickles directory and the FILE_INSTANCES file already exist then
+        it reads the instances information from it.
+
+        :rtype: list
+        :return: A list of  :class:`boto.ec2.instance.Instance`
+        """
+
+        if not os.path.isfile(FULL_FILE_INSTANCES_PATH):
+                file_instances = open(FULL_FILE_INSTANCES_PATH, FILE_WRITE_BYTES)
+                instances = connection.get_only_instances()
+                pickle.dump(instances, file_instances)
+        else:
+                file_instances = open(FULL_FILE_INSTANCES_PATH, FILE_READ_BYTES)
+                instances = pickle.load(file_instances)
+        file_instances.close()
+
+        return instances
+
+if __name__ == "__main__":
+        if not os.path.isdir(DIRECTORY_PICKLES):
+                os.mkdir(DIRECTORY_PICKLES)
+
+        connection = boto.connect_ec2()
+        instances = get_instances()
+
+        for instance in instances:
+                if INSTANCE_NAME_TAG in instance.tags:
+                        print(instance.tags[INSTANCE_NAME_TAG])
+                else:
+                        print("Instance has no InstanceName tag")
